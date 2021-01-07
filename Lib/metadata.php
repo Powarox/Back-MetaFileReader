e4ab89a749804791865fa5178a7a1dc39ff31cc6
Powarox2159 <moriniere.robin1877@gmail.com>
2021-01-07 18:20:59 +0100

    Start write function

--- Lib/metadata.php
+++ Lib/metadata.php
@@ -3,35 +3,36 @@
 namespace Robin\Lib;
 
 class metadata {
+    protected $file;
 
     // Constructor
-    public function __construct(){
-
+    public function __construct($file){
+        $this->file = $file;
     }
 
 // Return toutes les méta d'un fichier
-    public function getMeta(){
-
+    public function getMeta($file){
+        return $meta;
     }
 
 // Return les différent type de méta d'un fichier
-    public function getMetadataType(){
-
+    public function getMetadataType($file){
+        return $typeMeta;
     }
 
 // Return toutes les méta d'un certain type
-    public function getMetaOfType(){
-
+    public function getMetaOfType($file, $type){
+        return $meta;
     }
 
 // Add meta dans un fichier
-    public function setMeta(){
+    public function setMeta($file, $meta){
 
     }
 
 // Sort meta par type
-    public function sortedMeta(){
-
+    public function sortMeta($meta){
+        return $sortedMeta;
     }
 
 // Modifie les méta d'un fichier
@@ -40,18 +41,18 @@ class metadata {
     }
 
 // Save meta dans un fichier json
-    public function saveMetaJsonFile(){
+    public function saveMetaJsonFile($folder, $name, $meta){
 
     }
 
 // Put meta dans un fichier json
-    public function putMetaJsonFile(){
+    public function putMetaJsonFile($file, $meta){
 
     }
 
 // Suppre les meta en doublons
-    public function suppressMetaDouble(){
-
+    public function suppressMetaDouble($meta){
+        return $metaClean;
     }
 
 }
