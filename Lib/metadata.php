de061af3c7d998cc26c979559ed1cbb8461acce2
Powarox2159 <moriniere.robin1877@gmail.com>
2021-01-07 18:04:01 +0100

    Update

--- Lib/metadata.php
+++ Lib/metadata.php
@@ -1,1 +1,55 @@
 <?php
+
+class metadata {
+
+    // Constructor
+    public function __construct(){
+
+    }
+
+// Return toutes les méta d'un fichier
+    public function getMeta(){
+
+    }
+
+// Return les différent type de méta d'un fichier
+    public function getMetadataType(){
+        
+    }
+
+// Return toutes les méta d'un certain type
+    public function getMetaOfType(){
+
+    }
+
+// Add meta dans un fichier
+    public function setMeta(){
+
+    }
+
+// Sort meta par type
+    public function sortedMeta(){
+
+    }
+
+// Modifie les méta d'un fichier
+    public function setMetaModify(){
+
+    }
+
+// Save meta dans un fichier json
+    public function saveMetaJsonFile(){
+
+    }
+
+// Put meta dans un fichier json
+    public function putMetaJsonFile(){
+
+    }
+
+// Suppre les meta en doublons
+    public function suppressMetaDouble(){
+
+    }
+
+}
