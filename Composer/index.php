027cb4567309091f9a8fd726e824396aaa9aa7dc
Powarox2159 <moriniere.robin1877@gmail.com>
2021-01-08 18:55:11 +0100

    Add test composer

---
+++ Composer/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require "vendor/autoload.php";
+
+use \Michelf\Markdown;
+
+echo Markdown::defaultTransform("salut les gens **markdown**");
+
+ ?>
---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8f0851a2eb3fa11e0d1cff3b2941a5ca9cffc452',
+    'name' => 'test/composer',
+  ),
+  'versions' => 
+  array (
+    'test/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8f0851a2eb3fa11e0d1cff3b2941a5ca9cffc452',
+    ),
+  ),
+);
