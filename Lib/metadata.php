35b9efc8489a21cc91a33618ace1541a9ff057ae
Powarox2159 <moriniere.robin1877@gmail.com>
2021-01-11 17:23:53 +0100

    Update

--- Lib/metadata.php
+++ Lib/metadata.php
@@ -10,6 +10,7 @@ class Metadata {
         $this->file = $file;
     }
 
+
 // Return toutes les méta d'un fichier // Extraction Métadonnée
     public function getMeta($file){
         $data = shell_exec("exiftool -json ".$file);
@@ -17,30 +18,36 @@ class Metadata {
         return $metaData;
     }
 
+
 // Return les différent type de méta d'un fichier
     public function getMetadataType($file){
         return $typeMeta;
     }
+
 
 // Return toutes les méta d'un certain type
     public function getMetaOfType($file, $type){
         return $meta;
     }
 
+
 // Add meta dans un fichier
     public function setMeta($file, $meta){
 
     }
+
 
 // Sort meta par type
     public function sortMeta($meta){
         return $sortedMeta;
     }
 
+
 // Modifie les méta d'un fichier
     public function setMetaModify(){
 
     }
+
 
 // Save meta dans un fichier json // Créer un fichier contenant les métadata
     public function saveMetaJsonFile($folder, $name, $meta){
@@ -50,15 +57,18 @@ class Metadata {
         fclose($metaTxt);
     }
 
+
 // Put meta dans un fichier json
     public function putMetaJsonFile($file, $meta){
 
     }
 
+
 // Suppre les meta en doublons
     public function suppressMetaDouble($meta){
         return $metaClean;
     }
+
 
 // Gestion des erreurs
     public function getErr1(){
