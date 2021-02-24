8a0f6e48af89d2ce4263eeb55c5e9f3435156113
Powarox2159 <moriniere.robin1877@gmail.com>
2021-02-24 17:45:10 +0100

    Add composer for use library online

---
+++ TestApp/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0f1578133b8b5fcc8c21bdbb7afcadd92be4db38',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f1578133b8b5fcc8c21bdbb7afcadd92be4db38',
+    ),
+    'robindev/metadata' => 
+    array (
+      'pretty_version' => 'v1.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9f40b78ad5278fdf73e20004213c26a1a81b7a48',
+    ),
+  ),
+);
