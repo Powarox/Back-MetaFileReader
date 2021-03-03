206d5d90c4ef585780232068704fbad3e24072a6
Powarox2159 <moriniere.robin1877@gmail.com>
2021-03-03 14:34:42 +0100

    Update

---
+++ TestApp/Lib/Class/GestionExiftool.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TestApp\Lib\Class;
+
+class GestionExiftool {
+
+}
+
+// Verifier si exiftool est installé sur le serveur
+// si oui alors l'utiliser et trouver son path
+// sinon l'intégrer depuis notre lib / ou use online
+
+// Trouver moyen de configurer systeme pour que la lib use soit
+    // version fournis dans la lib
+    // version dispo sur serv
--- TestApp/Lib/Class/Utilitaire.php
+++ TestApp/Lib/Class/Utilitaire.php
@@ -1,3 +1,3 @@
 <?php
 
-namespace TestApp\Lib;
+namespace TestApp\Lib\Class;
